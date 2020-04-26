f2e6ad4d789e33cdfa3e07e164b7f010088261fe
Rachel Nguyen <35277830+racheljn@users.noreply.github.com>
2020-04-26 16:51:24 -0500

    Delete helloNames.c

--- demo/helloNames.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-   // printf() displays the string inside quotation
-
-   printf("Hello, %s, %s, and %s!", argv[1], argv[2], argv[3]);
-   return 0;
-}
