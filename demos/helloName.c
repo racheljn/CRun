e3978232493b3380ddbe2e064115a21686ca13a9
Sam gibson <samgibson@sams-mbp.attlocal.net>
2020-04-26 15:36:46 -0700

    added newlines to test files

--- demos/helloName.c
+++ demos/helloName.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char *argv[]) {
    // printf() displays the string inside quotation
-   printf("Hello, %s!", argv[1]);
+   printf("Hello, %s!\n", argv[1]);
    return 0;
 }
--- demos/helloNames.c
+++ demos/helloNames.c
@@ -2,6 +2,6 @@
 int main(int argc, char *argv[]) {
    // printf() displays the string inside quotation
 
-   printf("Hello, %s, %s, and %s!", argv[1], argv[2], argv[3]);
+   printf("Hello, %s, %s, and %s!\n", argv[1], argv[2], argv[3]);
    return 0;
 }
