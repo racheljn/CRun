1a7080223d7fdb4376d55a28dcc31425ae8faa7f
Sam gibson <samgibson@sams-mbp.attlocal.net>
2020-04-26 15:07:47 -0700

    failing example

---
+++ demos/failure.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+int main(int argc, char *argv[]) {
+   // printf() displays the string inside quotation
+   int x = 5;
+   char y = 'y';
+   notARealCall(x, y);
+   return 0;
+}
