5f2e35b4c104dadf7f58e7abfe35d05b36572e56
Sam gibson <samgibson@sams-mbp.attlocal.net>
2020-04-26 15:54:45 -0700

    failure file updates

--- demos/failure.c
+++ demos/failure.c
@@ -2,7 +2,8 @@
 int main(int argc, char *argv[]) {
    // printf() displays the string inside quotation
    int x = 5;
-   char y = 'y';
-   notARealCall(x, y);
+   char y = 'y'
+   printf("%d\n", x+y);
+   //notARealCall(x, y);
    return 0;
 }
